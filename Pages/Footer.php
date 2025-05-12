504a50f127278e8ca0bc8a5a0177f3b3ba1a5a98
KaungHtetKyaw2001 <102726297+KaungHtetKyaw2001@users.noreply.github.com>
2025-05-12 23:06:36 +0700

    Add files via upload

---
+++ Pages/Footer.php
@@ -0,0 +1,9 @@
+<footer>
+<a href="#"><img src="../images/facebook.png" class="facebook"></a>
+<a href="#"><img src="../images/Instagram.png" class="instagram"></a>
+<a href="#"><img src="../images/twitter.png" class="twitter"></a>
+<br>
+@Copyrighted 2019 by Kaung Htet Kyaw
+No-2093134
+</footer>
+</html>
